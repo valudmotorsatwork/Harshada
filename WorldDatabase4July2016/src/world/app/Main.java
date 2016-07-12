a8283c6f38c50c201064c93f252b73de2d4c3d06
HGonge <HGonge@VCPL1115CPU92.co.valudconsulting.com>
2016-07-12 10:08:42 +0530

    World Database 4july2016

---
+++ WorldDatabase4July2016/src/world/app/Main.java
@@ -0,0 +1,265 @@
+package world.app;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import world.dao.CityDAO;
+import world.domain.City;
+import world.view.CityTableModel;
+
+public class Main extends JFrame
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private CityDAO cityDao = new CityDAO();
+	public static final String APP_TITLE ="City Browser";
+	private CityTableModel tableModel;
+	private JTextField cityNameInput;
+	private JTextField cityNameSearchInput;
+	private JTextField cityDisrictInput;
+	private JTextField cityCountryCodeInput;
+	private JTextField cityPopulationInput;
+	private JButton addButton;
+	private JButton searchButton;
+	private JButton deleteButton;
+	private JButton updateButton;
+	JTable resultTable = new JTable();
+	JScrollPane scrollPane = new JScrollPane(resultTable);
+	public Main() 
+	{
+		super(APP_TITLE);
+		List<City> cityList = cityDao.loadCities();
+		tableModel= new CityTableModel();
+		tableModel.setCities(cityList);
+		setLayout(new BorderLayout(4, 4));
+		add(searchFieldsPanel(), BorderLayout.NORTH);
+		add(addFieldsPanel(), BorderLayout.SOUTH);
+		resultTable = new JTable(tableModel);
+		JScrollPane scrollPane = new JScrollPane(resultTable);
+		add(scrollPane, BorderLayout.CENTER);
+		addListners();
+		pack();
+		setVisible(true);
+	}
+//Insert Fields Panel
+private JPanel addFieldsPanel() 
+{
+	JPanel inputFieldsPanel = new JPanel();
+	inputFieldsPanel.setLayout(new FlowLayout());
+	JLabel cityLabel = new JLabel("City Name :");
+    inputFieldsPanel.add(cityLabel);
+    cityNameInput = new JTextField(10);
+    inputFieldsPanel.add(cityNameInput);
+    JLabel districtLabel = new JLabel("District :");
+    inputFieldsPanel.add(districtLabel);
+    cityDisrictInput = new JTextField(10);
+    inputFieldsPanel.add(cityDisrictInput);
+    JLabel countryLabel = new JLabel("Country :");
+    inputFieldsPanel.add(countryLabel);
+    cityCountryCodeInput = new JTextField(10);
+    inputFieldsPanel.add(cityCountryCodeInput);
+    JLabel populationLabel = new JLabel("Population :");
+    inputFieldsPanel.add(populationLabel);
+    cityPopulationInput = new JTextField(10); 
+    inputFieldsPanel.add(cityPopulationInput);
+    inputFieldsPanel.add(addControlPanel());
+    return inputFieldsPanel;
+}
+//Search Fields Panel
+private JPanel searchFieldsPanel() 
+{
+	JPanel inputFieldsPanel = new JPanel();
+	inputFieldsPanel.setLayout(new FlowLayout());
+	JLabel cityLabel = new JLabel("City Name :");
+    inputFieldsPanel.add(cityLabel);
+    cityNameSearchInput = new JTextField(10);
+    inputFieldsPanel.add(cityNameSearchInput);
+    inputFieldsPanel.add(searchControlPanel());
+    return inputFieldsPanel;
+}
+//Save,Delete and Update Control Panel
+private JPanel addControlPanel() 
+{
+	JPanel contolPanel = new JPanel();
+	contolPanel.setLayout(new BoxLayout(contolPanel, BoxLayout.X_AXIS));
+	addButton = new JButton("Save");
+	contolPanel.add(addButton);
+	deleteButton = new JButton("Delete");
+	contolPanel.add(deleteButton);
+	updateButton = new JButton("Update");
+	contolPanel.add(updateButton);
+	return contolPanel;
+}
+// Search Control Panel
+private JPanel searchControlPanel() 
+{
+	JPanel contolPanel = new JPanel();
+	contolPanel.setLayout(new BoxLayout(contolPanel, BoxLayout.X_AXIS));
+	searchButton = new JButton("Search");
+	contolPanel.add(searchButton);
+	return contolPanel;
+}
+// Reset the text fields
+private void resetFields() 
+{
+	cityNameInput.setText("");
+	cityDisrictInput.setText("");
+	cityCountryCodeInput.setText("");
+	cityPopulationInput.setText("");
+}
+private void addListners() 
+{
+	// Action Listener for Search method
+	searchButton.addActionListener(new ActionListener() 
+	{
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			try 
+			{
+				List<City> cities =null;
+				String search=cityNameSearchInput.getText();
+				if(search.equals(""))
+				search=" ";	
+				if(cityNameSearchInput.getText().equals(""))
+				cities = cityDao.loadCities();
+				else
+				cities = cityDao.findByName(search);
+				if(cities.size()==0)
+				{
+					JOptionPane.showMessageDialog (null, "No Records Found", "Information", JOptionPane.INFORMATION_MESSAGE);
+				}
+				tableModel.setCities(cities);
+				
+			}
+			catch (Exception e) 
+			{
+				System.out.println(e);
+				e.printStackTrace();
+			}
+		}
+	});
+	// Action Listener for Save method
+	addButton.addActionListener(new ActionListener() 
+	{
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			try 
+			{
+				City city = new City();
+				city.setCityName(cityNameInput.getText());
+				city.setCityDistrict(cityDisrictInput.getText());
+				city.setCityCountryCode(cityCountryCodeInput.getText());
+				city.setCityPopulation(Integer.parseInt(cityPopulationInput.getText()));
+				cityDao.save(city);
+				resetFields();
+				JOptionPane.showMessageDialog (null, "Record Saved Successful", "Information", JOptionPane.INFORMATION_MESSAGE);
+			}
+			catch (Exception e) 
+			{
+				System.out.println(e);
+				e.printStackTrace();
+			}
+		}
+	});
+	// Action Listener for Delete method
+	deleteButton.addActionListener(new ActionListener() 
+	{
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			try 
+			{
+				List<City> cities =null;
+				City city1 =  cityDao.findByName(cityNameInput.getText()).get(0);
+				int id = city1.getCityId();
+				cityDao.delete(id);
+				cities = cityDao.loadCities();
+				tableModel.setCities(cities);
+				JOptionPane.showMessageDialog (null, "Record Delete Successful", "Information", JOptionPane.INFORMATION_MESSAGE);
+			}
+			catch (Exception e) 
+			{
+				System.out.println(e);
+				e.printStackTrace();
+			}
+		}
+	});
+	// Action Listener for Update method 
+	updateButton.addActionListener(new ActionListener() 
+	{
+		public void actionPerformed(ActionEvent arg0) 
+		{
+			try 
+			{
+				City city = new City();
+				City city1 =  cityDao.findByName(cityNameInput.getText()).get(0);
+				int id = city1.getCityId();
+				city.setCityName(cityNameInput.getText());
+				city.setCityDistrict(cityDisrictInput.getText());
+				city.setCityCountryCode(cityCountryCodeInput.getText());
+				city.setCityPopulation(Integer.parseInt(cityPopulationInput.getText()));
+				city.setCityId(id);
+				cityDao.update(city);
+				JOptionPane.showMessageDialog (null, "Record Updated Successful", "Information", JOptionPane.INFORMATION_MESSAGE);
+			}
+			catch (Exception e) 
+			{
+				System.out.println(e);
+				e.printStackTrace();
+			}
+		}
+	});
+	// retrieve the selected record from Jtable
+	resultTable.getSelectionModel().addListSelectionListener(new ListSelectionListener()
+	{
+        public void valueChanged(ListSelectionEvent event) 
+        {
+        	try
+        	{
+        		String str=" ";
+        		boolean b=isEmpty(resultTable);
+        		if(!b)
+        		{
+        			str=resultTable.getValueAt(resultTable.getSelectedRow(), 0).toString();
+        		}
+        		List<City> cities=cityDao.findByName(str);
+        		if(cities.size()>0)
+        		{
+        			cityNameInput.setText(""+cities.get(0).getCityName());
+        			cityDisrictInput.setText(""+cities.get(0).getCityDistrict());
+        			cityCountryCodeInput.setText(""+cities.get(0).getCityCountryCode());
+        			cityPopulationInput.setText(""+cities.get(0).getCityPopulation());
+        		}
+        	}
+        	catch(Exception e)
+        	{
+        		e.printStackTrace();
+        	}
+        }
+    });
+	}
+	//Checking Jtable values null or not
+	public static boolean isEmpty(JTable jTable) 
+	{
+	    if (jTable != null && jTable.getModel() != null) 
+	    {
+	        return jTable.getModel().getRowCount()<=0?true:false;
+	    }
+	    return false;
+	}
+
+}
---
+++ WorldDatabase4July2016/src/world/dao/CityDAO.java
@@ -0,0 +1,146 @@
+package world.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import world.db.DBConnection;
+import world.domain.City;
+
+public class CityDAO 
+{
+	Connection conn=null;
+	public List<City> findByName(String name) 
+	{
+		List<City> results = new ArrayList<City>();
+		if(name==null)
+		return results;
+		try 
+		{
+			conn=DBConnection.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement( "SELECT * FROM CITY WHERE NAME=?" );
+			pstmt.setString(1, name);
+			ResultSet rs = pstmt.executeQuery( );
+			while (rs.next())
+			results.add(resultToCity(rs));
+			rs.close();
+			conn.close();
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+		return results;
+	}
+	/**
+	 * Copy current ResultSet row into a City object.
+	 * @param rs  ResultSet with a current row
+	 * @return a City object containing resultSet data for one row
+	 */
+	private City resultToCity( ResultSet rs ) 
+	{
+		City city = new City();
+		try 
+		{
+			city.setCityId(rs.getInt("id"));
+			city.setCityName(rs.getString("name"));
+			city.setCityDistrict(rs.getString("district"));
+			city.setCityPopulation(rs.getInt("population"));
+			city.setCityCountryCode(rs.getString("countrycode"));
+		}
+		catch (SQLException sqle) 
+		{
+			sqle.printStackTrace();
+		}
+		return city;
+	}
+
+	public List<City> loadCities() 
+	{
+		List<City> results = new ArrayList<City>();
+		try 
+		{
+			conn=DBConnection.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement( "SELECT * FROM CITY");
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next())
+			results.add(resultToCity(rs));
+			rs.close();
+			conn.close();
+		}
+		catch (SQLException sqle) 
+		{
+			sqle.printStackTrace();
+		}
+		return results;
+	}
+	
+	public void save(City city) throws SQLException 
+	{
+		try 
+		{
+			String maxId="select max(id) from city";
+			conn=DBConnection.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(maxId);
+			ResultSet rs=pstmt.executeQuery();
+			int maxCityId=0;
+			if(rs.next()){
+				maxCityId = rs.getInt(1);
+				}
+			rs.close();
+						
+			String sql = "INSERT INTO city (id,name,countrycode,district,population) VALUES(?,?,?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, maxCityId+1);
+			pstmt.setString(2, city.getCityName());
+			pstmt.setString(3, city.getCityCountryCode());
+			pstmt.setString(4, city.getCityDistrict());
+			pstmt.setInt(5, city.getCityPopulation());
+			pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+		}
+		catch (Exception sqle) 
+		{
+			sqle.printStackTrace();
+		}
+	}
+	public void delete(int value) throws SQLException 
+	{
+		try 
+		{
+			conn=DBConnection.getConnection();
+			Statement pstmt = conn.createStatement();
+			pstmt = conn.createStatement();
+			pstmt.execute("DELETE FROM CITY WHERE ID ="+value);
+			pstmt.close();
+			conn.close();
+		}
+		catch (Exception sqle) 
+		{
+			sqle.printStackTrace();
+		}
+	}
+	
+	public void update(City city) throws SQLException 
+	{
+		try 
+		{
+			conn=DBConnection.getConnection();
+			PreparedStatement ps = conn.prepareStatement("UPDATE CITY SET name = ?, countrycode = ?,district=?, population=? WHERE id = ?");
+		    ps.setString(1,city.getCityName());
+		    ps.setString(2, city.getCityCountryCode());
+		    ps.setString(3, city.getCityDistrict());
+		    ps.setInt(4,city.getCityPopulation());
+		    ps.setInt(5,city.getCityId());
+		    ps.executeUpdate();
+		    ps.close();
+		    conn.close();
+		}
+		catch (Exception sqle) 
+		{
+			sqle.printStackTrace();
+		}
+	}
+}
