cd54aeb16d58f1d74b12c1a6240fa6fa1da04da7
HGonge <HGonge@VCPL1115CPU92.co.valudconsulting.com>
2016-07-12 10:03:49 +0530

    Assignment4 Collection

---
+++ Assignment4(Collenction)28June2016/src/com/collection/impl/ImplementationClass.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.collection.impl;
+
+/**
+ * @author HGonge
+ * Date 6July2016 
+ */
+public class ImplementationClass {
+
+}
---
+++ Assignment4(Collenction)28June2016/src/com/collection/main/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.collection.main;
+
+/**
+ * @author HGonge
+ * Date: 6July2016
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
